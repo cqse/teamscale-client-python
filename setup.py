402d60dc16e699dba6afac52433a66f3ab264717
Thomas Kinnen <kinnen@cqse.eu>
2016-01-14 15:09:06 +0100

    Version 0.0.5

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name = "teamscale-client",
-    version = "0.0.4",
+    version = "0.0.5",
     author = "Thomas Kinnen - CQSE GmbH",
     author_email = "kinnen@cqse.eu",
     description = ("A simple service client to interact with Teamscale's REST API."),
