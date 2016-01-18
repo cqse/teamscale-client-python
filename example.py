ae650888859a786a22f99f9b3a8fb96de3e03720
Thomas Kinnen <kinnen@cqse.eu>
2016-01-18 11:10:39 +0100

    More complete example

--- example.py
+++ example.py
@@ -1,8 +1,9 @@
 from __future__ import print_function
 
 import datetime
+import sys
 
-from teamscale_client import TeamscaleClient
+from teamscale_client import TeamscaleClient, FindingDescription
 
 TEAMSCALE_URL = "http://localhost:8080"
 
@@ -12,11 +13,37 @@ PASSWORD = "admin"
 PROJECT_NAME = "foo"
 
 if __name__ == '__main__':
+    client = TeamscaleClient(TEAMSCALE_URL, USERNAME, PASSWORD, PROJECT_NAME)
+
+    # Add a new group that will contain findings
+    response = client.add_findings_group("Group 1", "externals-.*")
+    print("Request result: %s" % (response.text,))
+
+    # Make Teamscale aware of a new findings type, which mappes to the previously
+    # created group
+    descriptions = [
+        FindingDescription("externals-1", "A test finding description", "RED")
+    ]
+    response = client.add_finding_descriptions(descriptions)
+    print("Request result: %s" % (response.text,))
+
+    # A manual step to add the new groups to existing analysis profiles has to be done.
+    python2 = sys.version_info[0] == 2
+    if python2:
+        raw_input("Please create the project or update the analysis profile used by the project to contain the new groups. Then Press ENTER to continue.")
+    else:
+        input("Please create the projetc or update the analysis profile used by the project to contain the new groups. Then Press ENTER to continue.")
+
+    # Update existing project to use new findings
+    response = client.update_findings_schema()
+    print("Request result: %s" % (response.text,))
+
+    # Upload findings to Teamscale
     findings = [
         {
             "findings": [
                 {
-                    "findingTypeId": "externals-2-type-2",
+                    "findingTypeId": "externals-1",
                     "message": "test",
                     "assessment": "RED",
                     "startLine": 1,
@@ -25,9 +52,8 @@ if __name__ == '__main__':
                     "endOffset": 26
                 }
             ],
-            "path": "src/main/java/junit/extensions/ActiveTestSuite.java"
+            "path": "src/Foo.java"
         }
     ]
-    client = TeamscaleClient(TEAMSCALE_URL, USERNAME, PASSWORD, PROJECT_NAME)
     response = client.upload_findings(findings, datetime.datetime.now(), "TestCommit", "test-partition")
     print("Request result: %s" % (response.text,))
