a1e37b5839c7e7fe60424ee7a8f41c29f2428b07
Thomas Kinnen <kinnen@cqse.eu>
2016-02-29 16:12:17 +0100

    Up Version to 1.0.01, to make up for the last broken commit message.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="teamscale-client",
-    version="0.0.10",
+    version="1.0.01",
     author="Thomas Kinnen - CQSE GmbH",
     author_email="kinnen@cqse.eu",
     description=("A simple service client to interact with Teamscale's REST API."),
