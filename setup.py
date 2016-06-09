c002e285ededd8e908441513855d432d761551f8
Fabian Streitel <karottenreibe@users.noreply.github.com>
2016-06-09 11:39:23 +0200

    Noncodefix (#15)
    * simplify non-code metric data classes to mirror server version
    * bump version number to 1.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="teamscale-client",
-    version="1.2.1",
+    version="1.2.2",
     author="Thomas Kinnen - CQSE GmbH",
     author_email="kinnen@cqse.eu",
     description=("A simple service client to interact with Teamscale's REST API."),
