3b0cb3f824c3101c89c1bb54c790f4f208c84cbc
Thomas Kinnen <kinnen@cqse.eu>
2016-06-09 11:40:33 +0200

    Updating version due to backwards compatible break

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="teamscale-client",
-    version="1.2.2",
+    version="1.3.0",
     author="Thomas Kinnen - CQSE GmbH",
     author_email="kinnen@cqse.eu",
     description=("A simple service client to interact with Teamscale's REST API."),
