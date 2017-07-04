4c92e1c6f7ecd5c05085d4869234e385aaca538b
Thomas Kinnen <kinnen@cqse.eu>
2017-07-04 15:39:39 +0200

    Bump version for 3.4.1 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="teamscale-client",
-    version="3.4.0",
+    version="3.4.1",
     author="Thomas Kinnen - CQSE GmbH",
     author_email="kinnen@cqse.eu",
     description=("A simple service client to interact with Teamscale's REST API."),
