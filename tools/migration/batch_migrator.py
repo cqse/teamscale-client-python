7efce701a9c1bd00290b9da925af57b2a319b685
pawelka <pawelka@cqse.eu>
2018-02-22 14:55:06 +0100

    Added logging%

--- tools/migration/batch_migrator.py
+++ tools/migration/batch_migrator.py
@@ -4,6 +4,7 @@ import json
 from pathlib import Path
 from task_migrator import TaskMigrator
 from blacklist_migrator import BlacklistMigrator
+from migrator_base import create_logger
 
 
 def main():
@@ -13,34 +14,33 @@ def main():
     parser.add_argument("config", help="The path to the config file. Needs to be in a specific format, "
                                        "see batch_config.template.")
     args = parser.parse_args()
-
+    logger = create_logger()
     config_file = Path(args.config)
     if not config_file.exists():
-        print("Config file does not exist")
+        logger.error("Config file does not exist")
 
     with config_file.open() as file:
         data = json.load(file)
         base_config = {x: data[x] for x in ["old_instance", "new_instance"]}
 
         for mapping in data["project_mappings"]:
-            migrate(base_config, mapping)
+            migrate(base_config, mapping, logger)
 
 
-def migrate(base_config, mapping):
+def migrate(base_config, mapping, logger):
     """ Migrates the blacklist and the tasks of between the projects defined in the mapping.
     The servers containing the project are defined in the base config.
     """
     if not all(key in mapping for key in ("from", "to")):
-        print("Project mapping is malformed: %s" % mapping)
+        logger.error("Project mapping is malformed: %s" % mapping)
         return None
 
     base_config["old_instance"]["project"] = mapping["from"]
     base_config["new_instance"]["project"] = mapping["to"]
 
-    print("Migrating from '{from}' to '{to}'".format(**mapping))
+    logger.info("Migrating from '{from}' to '{to}'".format(**mapping))
     BlacklistMigrator(base_config).migrate()
     TaskMigrator(base_config).migrate()
-    print("")
 
 
 if __name__ == "__main__":
