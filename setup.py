768dfccf6cc8477067d8a0a1c79023e0042cbdbc
Thomas Kinnen <kinnen@cqse.eu>
2018-08-27 14:48:06 -0700

    4.4.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="teamscale-client",
-    version="4.4.4",
+    version="4.4.5",
     author="Thomas Kinnen - CQSE GmbH",
     author_email="kinnen@cqse.eu",
     description=("A simple service client to interact with Teamscale's REST API."),
