0bcfdff5a9fe42a75826395772717842c56b8552
Jonas Schuhmacher <schuhmacher@cqse.eu>
2023-03-10 11:02:27 +0100

    adding useful links to the doc

--- docs/conf.py
+++ docs/conf.py
@@ -22,12 +22,7 @@ release = '9.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.githubpages'
-]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon', 'sphinx.ext.githubpages']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -40,7 +35,6 @@ pygments_style = 'sphinx'
 toc_object_entries_show_parents = 'domain'
 add_module_names = False
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
@@ -48,6 +42,42 @@ html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
 htmlhelp_basename = 'sphinxdoc'
 
+# -- Options for Theme 'sphinx_book_theme' -----------------------------------
+# https://sphinx-book-theme.readthedocs.io/en/latest/tutorials/get-started.html
+html_theme_options = {
+    "use_edit_page_button": True,
+    "use_source_button": True,
+    "use_issues_button": True,
+    "icon_links": [{
+        "name": "GitHub",
+        "url": "https://github.com/cqse/teamscale-client-python",
+        "icon": "fa-brands fa-square-github",
+        "type": "fontawesome"
+    }, {
+        "name": "Teamscale Documentation",
+        "url": "https://docs.teamscale.com/reference/python-client/",
+        "icon": "fa-solid fa-house",
+        "type": "fontawesome"
+    }, {
+        "name": "Teamscale REST API",
+        "url": "https://docs.teamscale.com/reference/rest-api/",
+        "icon": "fa-sharp fa-solid fa-gears",
+        "type": "fontawesome"
+    }, {
+        "name": "PyPi",
+        "url": "https://pypi.org/project/teamscale-client/",
+        "icon": "https://badge.fury.io/py/teamscale-client.svg",
+        "type": "url"
+    }]
+}
+
+html_context = {
+    "github_url": "https://github.com",
+    "github_user": "cqse",
+    "github_repo": "teamscale-client-python",
+    "github_version": "master",
+    "doc_path": "docs",
+}
 
 # -- Options for Python ------------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-the-python-domain
